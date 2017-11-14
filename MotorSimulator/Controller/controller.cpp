298f986807dff2c95627a36d01ddf8ba66f02eb4
Mauro de Lyon <maurodelyon@gmail.com>
2017-11-14 20:26:35 +0100

    context

--- MotorSimulator/Controller/controller.cpp
+++ MotorSimulator/Controller/controller.cpp
@@ -1,31 +1,48 @@
 #include <cmath>
 #include <string>
+#include <iostream>
 #include "controller.h"
 
+using namespace std;
+
 controller::controller() {
+    cout << "----- Motor simulator -----" << endl;
     this->Model = model();
+    set_desired_probe_position(3, 4);
+    cout << "get position" << endl;
+    cout << get_position() << endl;
+    cout << "get desired position" << endl;
+    cout << get_desired_position() << endl;
+    /*
+        de applicatie moet voor een coordinaat een desired length voor elke motor bepalen.
+            1. Locatie wordt ingevoerd
+            2. Desired length wordt berekend
+            3. Desired length wordt doorgestuurd naar de modules
+	            de modules berekenen vervolgens zelf hoe snel ze moeten bewegen
+            4. wachten op reacties
+    */
 }
 
-void controller::set_probe_position(double x, double y) {
+void controller::set_desired_probe_position(double x, double y) {
     for (motor_module &Motor : this->Model.Motors) {
         motor_module *motor_module = &Motor;
         motor_module->set_desired_length(sqrt(pow(motor_module->get_x() - x, 2) + pow(motor_module->get_y() - y, 2)));
     }
 }
 
-std::string controller::get_position() {
-    std::string info;
+string controller::get_position() {
+    string info;
     for (motor_module &Motor : this->Model.Motors) {
-        info += std::to_string(Motor.get_current_length());
+        info += to_string(Motor.get_current_length());
         info += '\n';
     }
     return info;
 }
 
-std::string controller::get_desired_position() {
-    std::string info;
+string controller::get_desired_position() {
+    string info;
     for (motor_module &Motor : this->Model.Motors) {
-        info += std::to_string(Motor.get_desired_length());
+        info += to_string(Motor.get_desired_length());
         info += '\n';
     }
     return info;
--- MotorSimulator/Controller/controller.h
+++ MotorSimulator/Controller/controller.h
@@ -1,12 +1,13 @@
 #pragma once
 
+#include <string>
 #include "../Model/model.h"
 
 class controller {
 public:
     controller();
 
-    void set_probe_position(double x, double y);
+    void set_desired_probe_position(double x, double y);
 
     std::string get_position();
     std::string get_desired_position();
