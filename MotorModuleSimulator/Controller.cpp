d6e7bec60f5244fbb9aeab57b6eccb09c69bf7f4
Mauro de Lyon <maurodelyon@gmail.com>
2017-11-16 16:27:55 +0100

    unoptimized

--- MotorModuleSimulator/Controller.cpp
+++ MotorModuleSimulator/Controller.cpp
@@ -1,20 +1,40 @@
 #include "Controller.h"
 
 Controller::Controller() {
-    MotorModuleSimulator motorModuleSimulator = MotorModuleSimulator(1, 0, 0);
-    motors.push_back(&motorModuleSimulator);
+    MotorModuleSimulator motorModuleSimulator1 = MotorModuleSimulator(1, 0, 0);
+    MotorModuleSimulator motorModuleSimulator2 = MotorModuleSimulator(2, 5, 0);
+    MotorModuleSimulator motorModuleSimulator3 = MotorModuleSimulator(3, 0, 5);
+    MotorModuleSimulator motorModuleSimulator4 = MotorModuleSimulator(4, 5, 5);
 
-    setProbePosition(30, 40);
+    motors.push_back(&motorModuleSimulator1);
+    motors.push_back(&motorModuleSimulator2);
+    motors.push_back(&motorModuleSimulator3);
+    motors.push_back(&motorModuleSimulator4);
 
-    std::cout << "Length: " << motorModuleSimulator.getLength() << std::endl;
+    setProbePosition(3, 4);
 
     while (true) {
+        // Wait 100 milliseconds
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        std::cout << motorModuleSimulator.getLength() << " - " << motorModuleSimulator.getDesiredLength() << std::endl;
-        if (motorModuleSimulator.getLength() == motorModuleSimulator.getDesiredLength())
-            break;
-    }
 
+        // Print status
+        for (MotorModuleSimulator *motor: motors) {
+            std::cout <<
+                      " ID: " << motor->getId() <<
+                      " - Length: " << motor->getLength() <<
+                      " - Desired: " << motor->getDesiredLength()
+                      << std::endl;
+        }
+        std::cout << std::endl;
+
+        // Check if finished
+        bool done = true;
+        for (MotorModuleSimulator *motor: motors) {
+            if (motor->getLength() != motor->getDesiredLength())
+                done = false;
+        }
+        if (done)break;
+    }
 }
 
 void Controller::setProbePosition(double x, double y) {
--- MotorModuleSimulator/MotorModuleSimulator.cpp
+++ MotorModuleSimulator/MotorModuleSimulator.cpp
@@ -15,11 +15,11 @@ void MotorModuleSimulator::runSim() {
             break;
         }
         if (length > desiredLength) {
-            setLength(length - 1);
+            setLength(length - 0.0001);
         } else if (length < desiredLength) {
-            setLength(length + 1);
+            setLength(length + 0.0001);
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::nanoseconds(1));
     }
 }
