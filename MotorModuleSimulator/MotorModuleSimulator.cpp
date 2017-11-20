eec0ba2cb5ffac9fc766dc1fd2eff0d7d8738f72
Mauro de Lyon <maurodelyon@gmail.com>
2017-11-20 19:00:02 +0100

    Arduino code

--- MotorModuleSimulator/MotorModuleSimulator.cpp
+++ MotorModuleSimulator/MotorModuleSimulator.cpp
@@ -11,6 +11,8 @@ MotorModuleSimulator::MotorModuleSimulator(int id, int x, int y, int z) {
 
 void MotorModuleSimulator::runSim() {
     while (true) {
+
+
         if (length == desiredLength) {
             std::cout << "Motor " << getId() << ": done" << std::endl;
             break;
@@ -23,4 +25,57 @@ void MotorModuleSimulator::runSim() {
 
         std::this_thread::sleep_for(std::chrono::nanoseconds(1));
     }
+}
+
+// start of Arduino code
+typedef enum {
+    LISTENING,
+    SETTING_LENGTH,
+    MOVING,
+    DONE
+} StateType;
+
+typedef struct {
+    StateType State;
+
+    void (*func)(void);
+} StateMachineType;
+
+StateMachineType StateMachine[] = {
+        {LISTENING,      Sm_Listening},
+        {DONE,           Sm_Done},
+        {SETTING_LENGTH, Sm_SettingLength},
+        {MOVING,         Sm_Moving}
+};
+
+StateType SmState = LISTENING;
+
+std::string Command;
+
+void loop() {
+    if (SmState < 4) {
+        (*StateMachine[SmState].func)();
+    } else {
+    }
+}
+
+void Sm_Listening(void) {
+    // wait for commands
+    // if command is received
+    // Set command variable
+    // Check if for correct module
+    // execute
+}
+
+void Sm_SettingLength(void) {
+    // Set correct length
+}
+
+
+void Sm_Moving(void) {
+    // Move rope to desired length
+}
+
+void Sm_Done(void) {
+    // transmit done and wait for received
 }
