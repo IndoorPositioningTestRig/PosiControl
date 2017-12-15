5ac7e37e68babe4ae7ba78d9df415e770c31ba5e
Mauro de Lyon <maurodelyon@gmail.com>
2017-12-15 15:38:03 +0100

    hardcoded test for communication handler

--- MotorModuleSimulator/main.cpp
+++ MotorModuleSimulator/main.cpp
@@ -1,6 +1,19 @@
 #include "Controller.h"
+#include "SerialPort.h"
+#include "CommunicationHandler.h"
+
+using namespace std;
+
+char *port_name = "\\\\.\\COM15";
+
+//String for incoming data
+char *incomingData;
 
 int main() {
-    Controller controller;
+//    Controller controller;
+
+    CommunicationHandler communicationHandler;
+//    communicationHandler.executeMove();
+    communicationHandler.setLength(1, 100, 100);
     return 0;
 }
