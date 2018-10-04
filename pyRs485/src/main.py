2d6a171186e9208a6af209c12026951418ce8a33
Stefan Mermans <stefanmermans99@gmail.com>
2018-10-04 17:26:31 +0200

    python rs485 start

---
+++ pyRs485/src/main.py
@@ -0,0 +1,22 @@
+import serial
+import serial.rs485
+
+PORT: str = "22"
+
+
+def main():
+    try:
+        ser = serial.Serial(port=PORT)
+    except serial.SerialException:
+        print("Failed to open port: " + PORT)
+        return
+
+    if not ser.is_open():
+        print("Serial port: " + PORT + " is not open")
+        return
+    ser.rs485_mode = serial.rs485.RS485Settings()
+    ser.write(b'hello')
+
+
+if __name__ == "__main__":
+    main()
