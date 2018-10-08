14c892f1a06c73c91f1f83f376bd0a073d22955b
Stefan Mermans <stefanmermans99@gmail.com>
2018-10-08 09:42:09 +0200

    ports

--- pyRs485/src/main.py
+++ pyRs485/src/main.py
@@ -1,10 +1,42 @@
 import serial
 import serial.rs485
+import sys
+import glob
 
 PORT: str = "com6"
 
 
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
 def main():
+    ports = serial_ports()
     try:
         ser = serial.Serial(port=PORT)
     except serial.SerialException:
