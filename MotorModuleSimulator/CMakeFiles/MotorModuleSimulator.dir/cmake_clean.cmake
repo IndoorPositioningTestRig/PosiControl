a5dfee152809ed60323f2d39c1637965a3e19be3
Stefan Mermans <stefanmermans99@gmail.com>
2018-11-09 11:20:48 +0100

    wip

---
+++ MotorModuleSimulator/CMakeFiles/MotorModuleSimulator.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MotorModuleSimulator.dir/main.cpp.o"
+  "CMakeFiles/MotorModuleSimulator.dir/Controller.cpp.o"
+  "CMakeFiles/MotorModuleSimulator.dir/RS485/SerialPort.cpp.o"
+  "CMakeFiles/MotorModuleSimulator.dir/RS485/CommunicationHandler.cpp.o"
+  "CMakeFiles/MotorModuleSimulator.dir/MotorModule.cpp.o"
+  "MotorModuleSimulator.pdb"
+  "MotorModuleSimulator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MotorModuleSimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- pyRs485/src/main.py
+++ pyRs485/src/main.py
@@ -2,16 +2,11 @@ import serial
 import serial.rs485
 from serial.rs485 import RS485
 import sys
-import wiringpi
 
 PORT = "/dev/ttyS0"
 
 
 def main():
-    wiringpi.wiringPiSetup()
-    wiringpi.pinMode(18, wiringpi.OUTPUT)
-    wiringpi.digitalWrite(18, wiringpi.LOW)
-
     try:
         ser = RS485(port=PORT)
         # ser = serial.Serial(port=PORT)
@@ -22,9 +17,12 @@ def main():
         print("Serial port: " + PORT + " is not open")
         return
     ser.rs485_mode = serial.rs485.RS485Settings()
-    print("Writing...")
-    ser.read(1)
-    sys.stdout.flush()
+    count = 0
+    while True:
+        print("Writing... " + str(count))
+        ser.write("Hello World")
+        sys.stdout.flush()
+        count += 1
 
 
 if __name__ == "__main__":
