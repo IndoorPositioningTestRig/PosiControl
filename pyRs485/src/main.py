587d09e801e0c18514f0d7479d06c024d3606aca
Stefan Mermans <stefanmermans99@gmail.com>
2018-11-09 11:55:16 +0100

    wip

--- pyRs485/src/main.py
+++ pyRs485/src/main.py
@@ -6,8 +6,35 @@ import sys
 PORT = "/dev/ttyS0"
 
 
+def read_loop(ser):
+    count = 0
+    while True:
+        read = ser.read(1)
+        print("read: " + str(read) + " " + str(count))
+        sys.stdout.flush()
+        count += 1
+
+
+def write_loop(ser):
+    count = 0
+    if ser.writable():
+        print("Writable!")
+    else:
+        print("Not writable...")
+
+    while True:
+        print("Writing... " + str(count))
+        ser.write(b"Hello World")
+        sys.stdout.flush()
+        count += 1
+
+
 def main():
-    # sys.argv
+    read = False
+    for arg in sys.argv:
+        if arg.lower() == "read":
+            read = True
+
     try:
         ser = RS485(port=PORT)
         # ser = serial.Serial(port=PORT)
@@ -18,14 +45,10 @@ def main():
         print("Serial port: " + PORT + " is not open")
         return
     ser.rs485_mode = serial.rs485.RS485Settings()
-    count = 0
-    while True:
-        read = ser.read(1)
-        print("read: " + str(read) + " " + str(count))
-        # print("Writing... " + str(count))
-        # ser.write(b"Hello World")
-        sys.stdout.flush()
-        count += 1
+    if read:
+        read_loop(ser)
+    else:
+        write_loop(ser)
 
 
 if __name__ == "__main__":
