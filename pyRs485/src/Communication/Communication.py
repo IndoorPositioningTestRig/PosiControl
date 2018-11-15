6d4a8a9bde385ab3080191cb6bea1e7340d2d762
Stefan Mermans <stefanmermans99@gmail.com>
2018-11-15 14:27:25 +0100

    wip

--- pyRs485/src/Communication/Communication.py
+++ pyRs485/src/Communication/Communication.py
@@ -54,13 +54,13 @@ class Communication:
         if read == START_BYTE:
             message = Message()
             read = self.ser.read(1)
-            message.sender = int(read)
+            message.sender = int(read, 16)
             read = self.ser.read(1)
-            message.target = int(read)
+            message.target = int(read, 16)
             read = self.ser.read(1)
-            message.message_type = int(read)
+            message.message_type = int(read, 16)
             read = self.ser.read(1)
-            message.length = int(read)
+            message.length = int(read, 16)
 
             index = 0
             while index < message.length:
