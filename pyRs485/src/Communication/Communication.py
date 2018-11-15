b10e4d0e59e5566aec6029e2dd21123547d5239d
Stefan Mermans <stefanmermans99@gmail.com>
2018-11-15 14:22:15 +0100

    wip

--- pyRs485/src/Communication/Communication.py
+++ pyRs485/src/Communication/Communication.py
@@ -50,7 +50,7 @@ class Communication:
 
     def read(self):
         read = self.ser.read()
-        # print("read: " + str(read))
+        print("read: " + str(read))
         if read == START_BYTE:
             message = Message()
             read = self.ser.read(1)
