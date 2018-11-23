ab5f6c9c86739b52290ab477391acfbe3273934a
Stefan Mermans <stefanmermans99@gmail.com>
2018-11-23 14:12:01 +0100

    post request

--- pyserver/endpoints/urls.py
+++ pyserver/endpoints/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('msg/<int:target>/<int:msg_type>/', views.index, name='index'),
 ]
--- pyserver/endpoints/views.py
+++ pyserver/endpoints/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
-def index(request):
+
+@csrf_exempt
+def index(request, target: int, msg_type: int):
     return HttpResponse("hello motherfucker")
 
-def post(request):
-    return HttpResponse(request)
-    
